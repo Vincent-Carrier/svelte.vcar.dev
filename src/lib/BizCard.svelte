74d51315731668e3ce3393eaf51f7e2c0f03c87a
Vincent-Carrier <vincentcarrier15@gmail.com>
2022-08-16 09:08:35 -0400

    Format

--- src/lib/BizCard.svelte
+++ src/lib/BizCard.svelte
@@ -2,7 +2,10 @@
 	import Github from '~icons/mdi/github'
 	import Twitter from '~icons/mdi/twitter'
 
-	const socials = [{ Icon: Github, href: 'https://'}, {Icon: Twitter, href: 'https://'}]
+	const socials = [
+		{ Icon: Github, href: 'https://' },
+		{ Icon: Twitter, href: 'https://' },
+	]
 </script>
 
 <div class="bizcard topography relative flex h-36 w-min gap-4 px-8 py-4">
@@ -13,12 +16,12 @@
 	/>
 	<div class="w-36" />
 	<div class="my-auto whitespace-nowrap text-right">
-		<div class="font-display text-stone-50 text-4xl">Vincent Carrier</div>
-		<div class="text-xl font-medium tracking-tight text-yellow-100 -mt-1">Web Developer</div>
-		<div class="flex w-full justify-end gap-2 mt-2">
+		<div class="font-display text-4xl text-stone-50">Vincent Carrier</div>
+		<div class="-mt-1 text-xl font-medium tracking-tight text-yellow-100">Web Developer</div>
+		<div class="mt-2 flex w-full justify-end gap-2">
 			{#each socials as social}
 				<a href={social.href}>
-					<svelte:component this={social.Icon} class="text-yellow-100 w-8 h-8" />
+					<svelte:component this={social.Icon} class="h-8 w-8 text-yellow-100" />
 				</a>
 			{/each}
 		</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,23 @@
 
 <article class="card flex flex-col gap-4 p-12">
 	<p class="mb-24 font-serif text-2xl leading-relaxed text-stone-700">
-		<strong class="mb-4 inline-block text-4xl">Hi there! 👋</strong><br />
-		I'm a Montréal-based web developer,<br />
-		<em class="mb-2 inline-block w-full origin-right -rotate-3 scale-105 transform text-right"
-			>a digital vagabond,</em
-		>
+		<strong class="mb-4 inline-block text-4xl">Hi there! 👋</strong>
+		<br />
+		I'm a Montréal-based web developer,
+		<br />
+		<em class="mb-2 inline-block w-full origin-right -rotate-3 scale-105 transform text-right">
+			a digital vagabond,
+		</em>
 		<span class="text-xl">a detail-obsessed</span>
-		<span class="text-3xl font-medium italic">pixel-pusher</span>,<br />
-		<span class="block text-center"
-			>and just generally
-			<span class="inline-block translate-y-4 translate-x-2 transform font-black text-3xl"
-				>kind of a lot.</span
-			></span
-		>
+		<span class="text-3xl font-medium italic">pixel-pusher</span>
+		,
+		<br />
+		<span class="block text-center">
+			and just generally
+			<span class="inline-block translate-y-4 translate-x-2 transform text-3xl font-black">
+				kind of a lot.
+			</span>
+		</span>
 	</p>
 	<div class="absolute -bottom-16 -right-16">
 		<Bizcard />
