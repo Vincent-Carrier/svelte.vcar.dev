22ca6333b97dc26a226149c6250db71f0c276bca
Vincent Carrier <vincentcarrier15@gmail.com>
2022-10-22 08:48:43 -0400

    Fix build error

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,44 @@
 <script lang="ts">
 	import ExternalLink from '~icons/heroicons-outline/external-link'
-	import type { PageData } from './$types'
-	export let data: PageData
+	import endossed from './img/endossed.png?w=720&webp'
+	import greeklearnsyou from './img/greeklearnsyou.png?w=720&webp'
+	import molsoft from './img/molsoft.png?w=720&webp'
+	import thirdwheel from './img/thirdwheel.png?w=720&webp'
+
+	let projects = [
+			{
+				name: 'molsoft.io',
+				description: 'Marketing website for the Molsoft eCommerce agency',
+				technologies: 'React, NextJS, TypeScript, Tailwind',
+				img: molsoft,
+				url: 'https://www.molsoft.io/',
+			},
+			{
+				name: 'Endossed',
+				description: 'Web app for Endossed, a FinTech startup',
+				technologies: 'React, TypeScript, Tailwind',
+				img: endossed,
+				url: 'https://my.endossed.com/',
+			},
+			{
+				name: 'ThirdWheel',
+				description: 'A dating app with a twist: others match you',
+				technologies: 'React, SASS, PostgreSQL, Node.js',
+				img: thirdwheel,
+				url: 'https://github.com/Vincent-Carrier/thirdwheel',
+			},
+			{
+				name: 'Greek Learns You',
+				description: "Learn Greek words you didn't know you already knew",
+				technologies: 'Svelte, Tailwind',
+				img: greeklearnsyou,
+				url: 'https://github.com/Vincent-Carrier/greeklearnsyou',
+			},
+		]
 </script>
 
 <div class="mx-auto max-w-5xl px-4 grid gap-6 grid-cols-1 lg:grid-cols-2 mt-8">
-	{#each data.projects as p}
+	{#each projects as p}
 		<a href={p.url} target="_blank" rel="noreferrer">
 			<article class="card rounded-xl w-full hover:shadow-xl transform hover:scale-105 transition-all">
 				<div class="relative">
